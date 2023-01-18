64a1033ae987fbe3c60a4a3fe106b1122d4decb1
bartek <smalecbartlomiej@gmail.com>
2023-01-18 15:24:30 +0100

    Added JMS config + simple model

---
+++ src/main/java/com/barteksmalec/sfgjms/model/HelloWorldMessage.java
@@ -0,0 +1,19 @@
+package com.barteksmalec.sfgjms.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class HelloWorldMessage implements Serializable {
+    static final long serialVersionUID = 6545282435217435602L;
+    private UUID id;
+    private String message;
+}
